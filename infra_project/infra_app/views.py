ad13dc66d0ee3cb4d231d3f11d612652a3c20baf
Sergey-Sinitsin <valmont999@gmail.com>
2022-09-18 17:07:45 +0500

    выгрузка на боевой сервер через workflow

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось! Выполнить деплой через workflow. ))))')
 
 
 def second_page(request):
